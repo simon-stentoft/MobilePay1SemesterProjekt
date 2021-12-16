f03ab844ba422a0316734555b14be7dc112d89ac
Sim0n <57576661+simon-stentoft@users.noreply.github.com>
2021-12-16 11:14:58 +0100

    lavet flere views

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,5 +1,32 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class Controller {
 
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    public void switchSceneToCreateUser(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("mobilepay-view.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void switchSceneToLogIn(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("LogInView.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,8 +10,8 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("mobilepay-view.fxml"));
-        primaryStage.setTitle("Hello World");
+        Parent root = FXMLLoader.load(getClass().getResource("startScreenView.fxml"));
+        primaryStage.setTitle("Easy pay");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
